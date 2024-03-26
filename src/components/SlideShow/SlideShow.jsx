f475fe8f17329c3ebac88dec7868f2de246c3e30
Rhamseys Garcia <rhamseysgarcia@MacBook-Pro.lan>
2024-03-26 04:42:49 -0700

    Updates

--- src/components/SlideShow/SlideShow.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SlideShow = () => {
-  return (
-    <div>SlideShow Import Slideshow Module from &apos;/portfolio V2.0/Clothingbrad_Mock&apos;</div>
-  )
-}
-
-export default SlideShow
