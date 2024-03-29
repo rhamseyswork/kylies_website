839ab004f1042966c1f02b24bde8894a0f6778dd
Rhamseys Papadopulos <129332988+rhamseyswork@users.noreply.github.com>
2024-03-29 13:24:14 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,9 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <Router>
       <App />
     </Router>
+  </React.StrictMode>
 );
