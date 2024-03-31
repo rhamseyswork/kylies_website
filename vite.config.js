4ef7f7445e3dd26b29d3f266a611a63238f264a0
Rhamseys Garcia <rhamseysgarcia@MacBook-Pro.local>
2024-03-30 23:19:58 -0700

    deploy fix

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   define: {
     'process.env': process.env
   },
-  base: "/",
+  base: "https://rhamseyswork.github.io/kylies_website/",
   server: {
     port: 3000, // Change to the desired port number
     proxy: {
