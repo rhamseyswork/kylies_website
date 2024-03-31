ffa89bf3bf0dbce4c5db88453dbaefa9dd1a7e55
Rhamseys Garcia <rhamseysgarcia@MacBook-Pro.lan>
2024-03-30 19:09:41 -0700

    master copy

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,27 @@
 import dotenv from 'dotenv';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // run package config
 dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), reactRefresh()],
   // define process env
   define: {
     'process.env': process.env
   },
-  base: "/"
+  base: "/",
+  server: {
+    port: 3000, // Change to the desired port number
+    proxy: {
+      // Proxy settings if needed
+    },
+    https: false, // Set to true if you want to enable HTTPS
+  },
+  build: {
+    minify: true,
+    sourcemap: true,
+  },
 });
